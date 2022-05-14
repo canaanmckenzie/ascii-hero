7dd10a6fd7a2df3ab30eecc66a41c809b18129c6
Canaan <cjmckenz@uvm.edu>
2022-05-14 13:40:58 -0400

    add visibility range as color change in dungeon

--- src/map.rs
+++ src/map.rs
@@ -16,7 +16,8 @@ pub struct Map{
     pub rooms: Vec<Rect>,
     pub width: i32,
     pub height: i32,
-    pub revealed_tiles: Vec<bool>
+    pub revealed_tiles: Vec<bool>,
+    pub visible_tiles: Vec<bool>
 }
 
 //2d system from rltk
@@ -84,7 +85,8 @@ impl Map {
         rooms: Vec::new(),
         width: 80,
         height: 50,
-        revealed_tiles: vec![false;80*50]
+        revealed_tiles: vec![false;80*50],
+        visible_tiles: vec![false;80*50]
     };
 
     const MAX_ROOMS: i32 = 30;
@@ -133,15 +135,24 @@ pub fn draw_map(ecs: &World, ctx: &mut Rltk){
     let mut x = 0;
 
     for (idx,tile) in map.tiles.iter().enumerate() {
+
         if map.revealed_tiles[idx] {
+            let glyph;
+            let mut fg;
                 match tile {
                     TileType::Floor => {
-                        ctx.set(x,y,RGB::from_f32(0.5,0.5,0.5),RGB::from_f32(0.,0.,0.), rltk::to_cp437('▒'));
+                        glyph = rltk::to_cp437('▒');
+                        fg = RGB::from_f32(0.5,0.5,0.5);
+                        //ctx.set(x,y,RGB::from_f32(0.5,0.5,0.5),RGB::from_f32(0.,0.,0.), rltk::to_cp437('▒'));
                     }
                     TileType::Wall => {
-                        ctx.set(x,y,RGB::from_f32(0.1,0.1,0.1), RGB::from_f32(0.,0.,0.), rltk::to_cp437('#'));
+                        glyph = rltk::to_cp437('#');
+                        fg = RGB::from_f32(0.0,0.3,0.0);
+                        //ctx.set(x,y,RGB::from_f32(0.1,0.1,0.1), RGB::from_f32(0.,0.,0.), rltk::to_cp437('#'));
                     }
                 }
+                if !map.visible_tiles[idx] {fg = fg.to_greyscale()}
+                ctx.set(x,y,fg, RGB::from_f32(0.,0.,0.),glyph);
             }
         //move coordinates
         x += 1;
--- src/visibility_system.rs
+++ src/visibility_system.rs
@@ -15,19 +15,23 @@ impl <'a>System<'a> for VisibilitySystem {
 		let (mut map,entities, mut viewshed, pos, player) = data;
 
 		for (ent,viewshed, pos) in (&entities, &mut viewshed, &pos).join(){
-			viewshed.visible_tiles.clear();
-			//call rltk field of view function from rltk::Point system use &*map to dereference then reference to unwrap Map from ecs
-			viewshed.visible_tiles = field_of_view(Point::new(pos.x,pos.y),viewshed.range,&*map);
-			//use vectors retarin function - lambda (iteratres over vector passing p as param)
-			//runs every frame and stores a list of tiles as long as inside map
-			viewshed.visible_tiles.retain(|p| p.x >= 0 && p.x < map.width && p.y >= 0 && p.y < map.height);
+			if viewshed.dirty {
+				viewshed.visible_tiles.clear();
+				//call rltk field of view function from rltk::Point system use &*map to dereference then reference to unwrap Map from ecs
+				viewshed.visible_tiles = field_of_view(Point::new(pos.x,pos.y),viewshed.range,&*map);
+				//use vectors retarin function - lambda (iteratres over vector passing p as param)
+				//runs every frame and stores a list of tiles as long as inside map
+				viewshed.visible_tiles.retain(|p| p.x >= 0 && p.x < map.width && p.y >= 0 && p.y < map.height);
 
-			//if entity is a player, reveal what they can see
-			let p: Option<&Player> = player.get(ent);
-			if let Some(_p) = p {
-				for vis in viewshed.visible_tiles.iter() {
-					let idx = map.xy_idx(vis.x,vis.y);
-					map.revealed_tiles[idx] = true;
+				//if entity is a player, reveal what they can see
+				let _p: Option<&Player> = player.get(ent);
+				if let Some(_p) = _p {
+					for t in map.visible_tiles.iter_mut() {*t = false};
+					for vis in viewshed.visible_tiles.iter() {
+						let idx = map.xy_idx(vis.x,vis.y);
+						map.revealed_tiles[idx] = true;
+						map.visible_tiles[idx] = true;
+					}
 				}
 			}
 		}
